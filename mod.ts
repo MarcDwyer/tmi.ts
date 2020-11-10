5babe490cec17cb600d19fa94e74ad8eeeaa739f
Marc Dwyer <marcdwyer@marcs-mbp.attlocal.net>
2020-11-09 20:00:47 -0600

    now exporting twitch api types

--- mod.ts
+++ mod.ts
@@ -1,4 +1,5 @@
 export { TwitchChat } from "./lib/twitch_chat.ts";
 export { TwitchAPI } from "./lib/twitch_api.ts";
 export { Channel } from "./lib/channel.ts";
+export * from "./lib/twitch_api_types.ts";
 export * from "./lib/twitch_data.ts";
